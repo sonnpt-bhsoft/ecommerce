1d504d92594208790af3dde5f7ed690f68886c16
sonnpt.bhsoft <sonnpt.bhsoft@gmail.com>
2020-12-14 03:57:59 +0700

    luong ga vcl

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Products, Navbar } from "./components";
+
+import { commerce } from "./lib/commerce";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+
   return (
     <div className="App">
-      Hello World
+      <Navbar />
+      <Products products={products}/>
     </div>
   );
 }
